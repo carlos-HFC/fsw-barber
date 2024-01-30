f52583d54de8bbf01f596ba1be481415afa90096
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-30 13:01:26 -0300

    feat: welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { format } from "date-fns";
 import { Header } from "./_components/header";
 
 export default function Home() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <section className="px-5 space-y-5">
+        <div>
+          <h2 className="text-xl font-bold">Olá Carlos!</h2>
+          <p className="capitalize text-sm">
+            {format(new Date(), "EEEE',' dd 'de' MMMM")}
+          </p>
+        </div>
+
+
+      </section>
+    </div>
   );
 }
