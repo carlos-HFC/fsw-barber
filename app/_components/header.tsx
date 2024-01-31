7ee2bf08ecbcf7137d6c7b8956676c403c6f2a4c
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-31 14:10:33 -0300

    feat: add signin and signout

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,10 +1,26 @@
-import { MenuIcon } from "lucide-react";
-import Image from "next/image";
+"use client";
 
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon, MenuIcon, UserCircle2Icon, UserIcon, XIcon } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 export function Header() {
+  const { data, status } = useSession();
+
+  async function handleLogin() {
+    await signIn("google");
+  }
+
+  async function handleLogout() {
+    await signOut();
+  }
+
   return (
     <Card>
       <CardContent className="px-5 py-6 flex items-center justify-between">
@@ -15,12 +31,91 @@ export function Header() {
           height={22}
         />
 
-        <Button
-          variant="outline"
-          size="icon"
-        >
-          <MenuIcon size={20} />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+            >
+              <MenuIcon size={20} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SheetHeader className="text-left border-b py-8 px-5 border-secondary flex flex-row justify-between items-center space-y-0">
+              <SheetTitle>Menu</SheetTitle>
+              <SheetClose asChild>
+                <XIcon size={20} />
+              </SheetClose>
+            </SheetHeader>
+
+            <div className="px-5">
+              {status === 'authenticated'
+                ? (
+                  <div className="flex justify-between py-6">
+                    <div className="flex items-center gap-3">
+                      <Avatar>
+                        <AvatarImage src={data.user?.image ?? ""} />
+                        <AvatarFallback>{data.user?.name}</AvatarFallback>
+                      </Avatar>
+                      <h2 className="font-bold">{data.user?.name}</h2>
+                    </div>
+
+                    <Button
+                      variant="secondary"
+                      size="icon"
+                      onClick={handleLogout}
+                    >
+                      <LogOutIcon size={20} />
+                    </Button>
+                  </div>
+                ) : (
+                  <div className="flex flex-col gap-3 py-6">
+                    <div className="flex items-center gap-2">
+                      <UserCircle2Icon
+                        size={40}
+                        className="stroke-gray-400"
+                      />
+                      <h2 className="font-bold">Olá. Faça seu login!</h2>
+                    </div>
+
+                    <Button
+                      variant="secondary"
+                      className="font-bold gap-2 justify-normal"
+                      onClick={handleLogin}
+                    >
+                      <LogInIcon size={16} />
+                      Fazer login
+                    </Button>
+                  </div>
+                )}
+
+              <div className="flex flex-col gap-3">
+                <Button
+                  variant="outline"
+                  className="justify-normal gap-2"
+                  asChild
+                >
+                  <Link href="/">
+                    <HomeIcon size={16} />
+                    Início
+                  </Link>
+                </Button>
+
+                <Button
+                  variant="outline"
+                  className="justify-normal gap-2"
+                  asChild
+                >
+                  <Link href="/bookings">
+                    <CalendarIcon size={16} />
+                    Agendamentos
+                  </Link>
+                </Button>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   );
--- app/_components/ui/button.tsx
+++ app/_components/ui/button.tsx
@@ -23,7 +23,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "size-10",
+        icon: "min-w-10 max-w-10 aspect-square",
       },
     },
     defaultVariants: {
