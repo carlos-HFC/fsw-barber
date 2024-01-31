f33f512c973ac04171105f3ae22f66d7438df75e
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-31 12:47:00 -0300

    feat: next auth setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+import NextAuth from 'next-auth';
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from 'next-auth/providers/google';
+
+import { db } from "@/app/_lib/prisma";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: String(process.env.GOOGLE_CLIENT_ID),
+      clientSecret: String(process.env.GOOGLE_CLIENT_SECRET)
+    })
+  ]
+});
+
+export {
+  handler as GET,
+  handler as POST,
+};
