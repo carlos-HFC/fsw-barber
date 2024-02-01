50e0a71a21fa47e84a8617e76a219ba467263ffc
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-01 10:42:58 -0300

    refactor: import locale per using

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 import { BarbershopItem } from "./_components/barbershop-item";
 import { BookingItem } from "./_components/booking-item";
@@ -18,7 +19,7 @@ export default async function Home() {
         <div>
           <h2 className="text-xl font-bold">Olá Carlos!</h2>
           <p className="capitalize text-sm">
-            {format(new Date(), "EEEE',' dd 'de' MMMM")}
+            {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
           </p>
         </div>
 
