59edbbfa7969debaa05d9a51d0b5423c84855a40
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-01 15:20:47 -0300

    refactor: update year in footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="w-full bg-secondary py-6 px-5 mt-auto">
-      <p className="text-gray-400 text-xs">&copy; 2023 Copyright <strong>FSW Barber</strong></p>
+      <p className="text-gray-400 text-xs">&copy; 2024 Copyright <strong>FSW Barber</strong></p>
     </footer>
   );
 }
