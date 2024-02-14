c082edac825ea1b050fc7fabc2546c7d5656bf93
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-14 13:39:44 -0300

    feat: update footer in desktop

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,7 +1,9 @@
 export function Footer() {
   return (
-    <footer className="w-full bg-secondary py-6 px-5 mt-auto">
-      <p className="text-gray-400 text-xs">&copy; 2024 Copyright <strong>FSW Barber</strong></p>
+    <footer className="w-full bg-secondary">
+      <div className="container py-6 px-5">
+        <p className="text-gray-400 text-xs">&copy; 2024 Copyright <strong>FSW Barber</strong></p>
+      </div>
     </footer>
   );
 }
