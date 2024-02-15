58ff13f059709a5d4c5a1178ac12b78e739bb1e7
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-15 09:27:13 -0300

    fix: fixing footer on page bottom

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="w-full bg-secondary">
+    <footer className="w-full bg-secondary mt-auto">
       <div className="container py-6 px-5">
         <p className="text-gray-400 text-xs">&copy; 2024 Copyright <strong>FSW Barber</strong></p>
       </div>
